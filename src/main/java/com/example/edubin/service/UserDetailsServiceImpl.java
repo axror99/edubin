da5ef03bbd6bcf3ea6fb041e5ea92752346eb079
Axror <ahror060799@gmail.com>
2023-05-03 09:39:47 +0500

    github action 10

--- src/main/java/com/example/edubin/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/edubin/service/UserDetailsServiceImpl.java
@@ -1,34 +1,26 @@
 package com.example.edubin.service;
 
 import com.example.edubin.enitity.UserEntity;
-import com.example.edubin.repository.UserRepository;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.text.MessageFormat;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UserDetailsServiceImpl implements AuditorAware<Integer> {
+public class UserDetailsServiceImpl implements AuditorAware<String> {
 
     @Override
-    @NonNull
-    public Optional<Integer> getCurrentAuditor() {
+    public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated() || !((authentication.getPrincipal()) instanceof UserEntity principal))
         {
             return Optional.empty();
         }
-        return Optional.of(principal.getId());
+        return Optional.of(principal.getUsername());
     }
 
 }
