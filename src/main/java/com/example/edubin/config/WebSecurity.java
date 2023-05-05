85496ead71b1b9bdc17c286064ac334bc4eef1bd
Axror <ahror060799@gmail.com>
2023-05-05 10:52:24 +0500

    github action 23

--- src/main/java/com/example/edubin/config/WebSecurity.java
+++ src/main/java/com/example/edubin/config/WebSecurity.java
@@ -36,7 +36,7 @@ public class WebSecurity {
                     CorsConfiguration configuration = new CorsConfiguration();
 //                    configuration.setAllowedOriginPatterns(List.of("*"));
 //                    configuration.setAllowedOriginPatterns(List.of("https://myedubin.netlify.app/"));
-                    configuration.setAllowedOriginPatterns(List.of("https://edubin-axror.herokuapp.com/apps"));
+                    configuration.setAllowedOriginPatterns(List.of("https://edubin-axror.herokuapp.com/"));
                     configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH"));
                     configuration.setAllowedHeaders(List.of("*"));
                     configuration.setAllowCredentials(true);
